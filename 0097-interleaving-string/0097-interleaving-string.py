e7eef0900fb792ba0ca320d8741441d8d397d581
prasath9843 <166198867+prasath9843@users.noreply.github.com>
2026-07-06 21:30:34 +0530

    Time: 50 ms (63.66%), Space: 19.5 MB (40.75%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        n, m = len(s1), len(s2)
+        
+        if n + m != len(s3):
+            return False
+        
+        dp = [[False] * (m + 1) for _ in range(n + 1)]
+        dp[0][0] = True
+        
+        for i in range(n + 1):
+            for j in range(m + 1):
+                if i > 0:
+                    dp[i][j] |= dp[i-1][j] and s1[i-1] == s3[i+j-1]
+                if j > 0:
+                    dp[i][j] |= dp[i][j-1] and s2[j-1] == s3[i+j-1]
+        
+        return dp[n][m]
